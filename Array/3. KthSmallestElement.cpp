09442a4ff01f9c2ea6e3e4ae31ab71e9f2d6dc79
tarunsarawgi <77830038+tarunsarawgi@users.noreply.github.com>
2023-02-28 23:21:44 +0530

    Create 3. KthSmallestElement.cpp

---
+++ Array/3. KthSmallestElement.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int kthSmallest(int arr[], int N, int K)
+{
+	sort(arr, arr + N);
+	return arr[K - 1];
+}
+
+int main()
+{
+	int arr[] = { 12, 3, 5, 7, 19 };
+	int N = sizeof(arr) / sizeof(arr[0]), K = 2;
+
+
+	cout << "K'th smallest element is " << kthSmallest(arr, N, K);
+	return 0;
+}
